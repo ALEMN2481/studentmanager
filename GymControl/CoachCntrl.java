d4166b7098257b1745c8b0bc191d5be67ad91de5
LkpFabio <fsbio9morales@gmai.com>
2025-10-01 15:17:58 -0500

    commit1

--- GymControl/CoachCntrl.java
+++ GymControl/CoachCntrl.java
@@ -1,5 +1,14 @@
 package GymControl;
 
 public class CoachCntrl {
-    
+
+    public CoachCntrl(String nombre, int id, int añosExperiencia) {
+
+    }
+
+    public double calcularSalario() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'calcularSalario'");
+    }
+
 }
--- GymControl/NutritionalGym.java
+++ GymControl/NutritionalGym.java
@@ -1,5 +1,41 @@
 package GymControl;
 
-public class NutritionalGym {
-    
+public class NutritionalGym extends CoachCntrl {
+    private String especialidad;
+    private double salarioBase;
+    private double bonoNutricionista;
+
+    public NutritionalGym(String nombre, int id, int añosExperiencia, String especialidad, double salarioBase,
+            double bonoNutricionista) {
+        super(nombre, id, añosExperiencia);
+        this.especialidad = especialidad;
+        this.salarioBase = salarioBase;
+        this.bonoNutricionista = bonoNutricionista;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public void setBonoNutricionista(double bonoNutricionista) {
+        this.bonoNutricionista = bonoNutricionista;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return salarioBase + bonoNutricionista;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Especialidad=" + especialidad + ", Salario=" + calcularSalario();
+    }
 }
